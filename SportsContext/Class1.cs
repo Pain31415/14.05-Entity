5cacc067e84e1d695666edbbafcf0b1b14ba78a1
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-05-17 19:02:46 +0300

    Add project files.

---
+++ SportsContext/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace SportsContext
+{
+    public class SportsContext : DbContext
+    {
+        public DbSet<SportsTeam> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SportsDb;Trusted_Connection=True");
+        }
+    }
+}
